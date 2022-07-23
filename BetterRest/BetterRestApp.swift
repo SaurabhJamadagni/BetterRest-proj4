5ef62858b73702077c1dc1bc5cc0e7ee17a415e1
Saurabh Jamadagni <saurabhjamadagni2002@gmail.com>
2022-07-23 17:23:51 +0530

    initial commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Admin on 23/07/22.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
